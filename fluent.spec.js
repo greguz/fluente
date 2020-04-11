6471b37e47e87ae063a300e3c27dfc7d95f0d13e
greguz <gregolettogiacomo@gmail.com>
2020-04-11 13:51:56 +0200

    Improve tests

--- fluent.spec.js
+++ fluent.spec.js
@@ -2,10 +2,35 @@ const test = require('ava')
 
 const fluente = require('./fluente.js')
 
-test('basic', t => {
-  t.plan(9)
+function noop () {
+  // nothing
+}
+
+test('interface', t => {
+  const instance = fluente({
+    fluent: {
+      a: noop
+    },
+    methods: {
+      b: noop
+    },
+    constants: {
+      c: 42
+    }
+  })
+
+  t.true(typeof instance.undo === 'function')
+  t.true(typeof instance.redo === 'function')
+  t.true(typeof instance.a === 'function')
+  t.true(typeof instance.b === 'function')
+  t.is(instance.c, 42)
+})
+
+test('lifecycle', t => {
+  t.plan(6)
 
   const instance = fluente({
+    historySize: 3,
     state: {
       value: 0
     },
@@ -15,48 +40,30 @@ test('basic', t => {
         return {
           value: this.value + value
         }
-      },
-      sub (value) {
-        t.pass()
-        return {
-          value: this.value - value
-        }
-      },
-      div (value) {
-        t.pass()
-        return {
-          value: this.value / value
-        }
-      },
-      mul (value) {
-        t.pass()
-        return {
-          value: this.value * value
-        }
       }
     },
     methods: {
       unwrap () {
         return this.value
       }
-    },
-    constants: {
-      hello: 'world'
     }
   })
 
-  t.true(typeof instance.undo === 'function')
-  t.true(typeof instance.redo === 'function')
-  t.is(instance.hello, 'world')
-
   const result = instance
-    .add(2)
-    .sub(4)
-    .mul(-1)
-    .div(2)
+    .add(1)
+    .add(1)
+    .add(1)
+    .add(1)
+    .add(1)
+    .undo(Infinity)
+    .redo()
     .unwrap()
 
-  t.is(result, 1)
+  t.is(result, 3)
+})
 
-  t.throws(() => instance.unwrap())
+test('locking', t => {
+  const instance = fluente()
+  instance.undo()
+  t.throws(instance.undo)
 })
